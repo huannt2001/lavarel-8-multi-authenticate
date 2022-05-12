b89cf7cfb9bb17d780694a8b3905cc36522020d2
Nguyen Trang Huan <nguyentranghuan638@gmail.com>
2022-05-12 21:57:02 +0700

    complete portion setup admin panel and frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -48,4 +48,43 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('admin.admin_master')
+@section('admin_content')
+    <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-100v">
+
+        <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white">
+            <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">starlight <span class="tx-info tx-normal">admin</span>
+            </div>
+            <div class="tx-center mg-b-60">Professional Admin Template Design</div>
+
+            <form method="POST" action="{{ isset($guard) ? url($guard . '/login') : route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Enter your email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <input type="password" id="password" name="password" class="form-control"
+                        placeholder="Enter your password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <a href="{{ route('admin.change.password') }}" class="tx-info tx-12 d-block mg-t-10">Forgot
+                        password?</a>
+                </div><!-- form-group -->
+                <button type="submit" class="btn btn-info btn-block">Sign In</button>
+            </form>
+
+
+        </div><!-- login-wrapper -->
+    </div><!-- d-flex -->
+@endsection
