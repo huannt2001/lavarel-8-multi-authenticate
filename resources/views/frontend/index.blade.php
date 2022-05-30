b8b7aa754a9a8df559667018f7f02d7a0d89a45e
Huan Nguyen <nguyentranghuan638@gmail.com>
2022-05-30 22:55:41 +0700

    test commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -192,7 +192,7 @@
                         <div class="tabbed_container">
                             <div class="tabs">
                                 <ul class="clearfix">
-                                    <li class="active">Featured 1</li>
+                                    <li class="active">Featured </li>
                                 </ul>
                                 <div class="tabs_line"><span></span></div>
                             </div>
